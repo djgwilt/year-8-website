a6db79d1eeca37e07b9a1ff458d59edbb59a4f63
Dave Gwilt <dave@Daves-MacBook-Pro.local>
2022-04-04 16:01:55 +0100

    First commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function changeIt() {
+   var para = document.getElementById("demo")
+   para.innerHTML = "You changed a paragraph using Javascript! If you have time after completing your site - there is a link in the comment at the bottom of the index.html file which gives lots of things you can do with Javascript. ";
+  para.style.backgroundColor = "black";
+  para.style.color = "white";
+  para.style.padding = "30px";
+  
+  
+}
+
